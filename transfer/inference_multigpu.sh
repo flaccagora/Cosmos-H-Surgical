65f08c6998ec81de3ca832354836c5be6937d672
flaccagora <matteo.nunziante@gmail.com>
2026-05-11 12:21:23 +0200

    update run inference and setup

--- transfer/inference_multigpu.sh
+++ transfer/inference_multigpu.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH -A IscrC_FLAC
 #SBATCH -p boost_usr_prod
+#SBATCH --exclusive
 #SBATCH --time 00:30:00      # format: HH:MM:SS
 #SBATCH -N 1                 # 1 node
-#SBATCH --ntasks-per-node=8  # 4 tasks out of 32
-#SBATCH --mem=64000          # memory per node out of 494000MB (481GB)
-#SBATCH --gres=gpu:1
-#SBATCH --qos=boost_qos_dbg
+#SBATCH --ntasks-per-node=4  # 4 tasks 
+#SBATCH --cpus-per-task=8    # 8 CPUs per task -> total 32 CPUs
+#SBATCH --mem=480000         # memory per node out of 494000MB (512GB)
+#SBATCH --gres=gpu:4
 #SBATCH --job-name=cosmos-h-inference
 #SBATCH --output=inference_output/inference_%j.out
 #SBATCH --error=inference_output/inference_%j.err
@@ -16,6 +17,7 @@ WORKDIR="/leonardo_work/IscrC_FLAC_0/Cosmos-H-Surgical/transfer"
 USERNAME=${USER}
 export HF_HUB_OFFLINE=1
 export COSMOS_HF_NO_DOWNLOAD=1
+export OMP_NUM_THREADS=8
 export HF_HOME="/leonardo_work/IscrC_FLAC_0/.HF_DOWNLOADS"
 
 echo "Loading necessary modules"
@@ -26,7 +28,9 @@ echo "Working directory: $WORKDIR"
 cd $WORKDIR
 
 ENV_COMMAND="source .venv/bin/activate"
-PYTHON_CMD="python examples/inference.py -i assets/coagulation_example/depth/coagulation_depth_spec.json -o outputs/depth"
+
+# multi GPU inference commands 
+PYTHON_CMD="torchrun --nproc_per_node=4 examples/inference.py -i assets/coagulation_example/depth/coagulation_depth_spec.json -o outputs/depth"
 CONTAINER_CMD="$ENV_COMMAND && $PYTHON_CMD"
 echo "Container command: $CONTAINER_CMD"
 
--- transfer/multiple_inference.sh
+++ transfer/multiple_inference.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH -A IscrC_FLAC
 #SBATCH -p boost_usr_prod
+#SBATCH --exclusive
 #SBATCH --time 00:30:00      # format: HH:MM:SS
 #SBATCH -N 1                 # 1 node
-#SBATCH --ntasks-per-node=8  # 4 tasks out of 32
-#SBATCH --mem=64000          # memory per node out of 494000MB (481GB)
-#SBATCH --gres=gpu:1
-#SBATCH --qos=boost_qos_dbg
+#SBATCH --ntasks-per-node=4  # 4 tasks 
+#SBATCH --cpus-per-task=8    # 8 CPUs per task -> total 32 CPUs
+#SBATCH --mem=480000         # memory per node out of 494000MB (512GB)
+#SBATCH --gres=gpu:4
 #SBATCH --job-name=cosmos-h-inference
 #SBATCH --output=inference_output/inference_%j.out
 #SBATCH --error=inference_output/inference_%j.err
@@ -17,6 +18,7 @@ USERNAME=${USER}
 export HF_HUB_OFFLINE=1
 export COSMOS_HF_NO_DOWNLOAD=1
 export HF_HOME="/leonardo_work/IscrC_FLAC_0/.HF_DOWNLOADS"
+export OMP_NUM_THREADS=8
 
 echo "Loading necessary modules"
 ml purge
@@ -26,8 +28,18 @@ echo "Working directory: $WORKDIR"
 cd $WORKDIR
 
 ENV_COMMAND="source .venv/bin/activate"
-PYTHON_CMD="python examples/inference.py -i assets/coagulation_example/depth/coagulation_depth_spec.json -o outputs/depth"
-CONTAINER_CMD="$ENV_COMMAND && $PYTHON_CMD"
+OUTPUT_DIR="outputs/batch"
+INPUT_1="assets/coagulation_example/depth/coagulation_depth_spec.json"
+INPUT_2="assets/needleGrasping_example/depth/needleGrasping_depth_spec.json"
+INPUT_3="assets/needlePuncture_guided_example/depth/needlePuncture_depth_spec.json"
+
+CMD_BATCH="torchrun --nproc_per_node=4 examples/inference.py \
+    -i $INPUT_1 \
+    -i $INPUT_2 \
+    -i $INPUT_3 \
+    -o $OUTPUT_DIR"
+
+CONTAINER_CMD="$ENV_COMMAND && $CMD_BATCH"
 echo "Container command: $CONTAINER_CMD"
 
 echo "Running inference inside the container"
@@ -39,5 +51,5 @@ singularity exec --nv --bind .:/workspace \
 
 
 date
-echo "Inference complete. Check outputs/depth for results."
+echo "Inference complete. Check outputs for results."
 
